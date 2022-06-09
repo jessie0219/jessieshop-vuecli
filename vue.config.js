76e68f991f12a18af2aa78621db664eb1fc84aaf
jessie0219 <nini880219@gmail.com>
2022-06-10 00:32:27 +0800

    刪除引入jquery語法

--- vue.config.js
+++ vue.config.js
@@ -1,33 +1,5 @@
-const webpack = require('webpack')
-
 module.exports = {
-  configureWebpack: {
-    plugins: [
-      new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery",
-        "windows.jQuery": "jquery"
-      })
-    ]
-  },
-
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/jessieshop-vuecli/'
-    : '/',
-
-  pluginOptions: {
-    vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
+    ? '/jessieshop-vuecli/' // test20200915 為 repo 名稱
+    : '/'
 }
-
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-
-  pluginOptions: {
-    vuetify: {}
-  }
-})
