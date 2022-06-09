e4907d352cb7bb7281ef01a389792c7c636a2f4b
jessie0219 <nini880219@gmail.com>
2022-06-10 00:41:18 +0800

    註解掉axios的import

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import axios from 'axios';
+// import axios from 'axios';
 
 const app = createApp(App);
-app.config.globalProperties.$axios = axios;
+// app.config.globalProperties.$axios = axios;
 
 
 app.use(router).use(store).mount('#app')
